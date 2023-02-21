70403995dbdbde51c33ba2136d9c1bd74c770074
Hai Ta Hong <haithz@nec.vn>
2023-02-21 12:02:31 +0700

    Refactor

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useAuthentication } from 'providers/AuthenticationProvider';
 
 const Header = (props) => {
+  const { t } = useTranslation();
+  const { logout } = useAuthentication();
+
+  //! Function
+  const onLogout = () => {
+    logout();
+  };
+
   return (
     <div className="header">
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/dashboard">Dashboard</Link>
+      <button onClick={onLogout}>Logout</button>
     </div>
   );
 };
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -6,28 +6,10 @@ import { useTranslation } from 'react-i18next';
 
 const HomePage = (props) => {
   const { t } = useTranslation();
-  const { logout } = useAuthentication();
-
-  const [data, loading, , refetch] = useGetListTodos(0);
-
-  //! Function
-  const onLogout = () => {
-    logout();
-  };
-
   //! Render
   return (
     <div>
       <h3>{t('message:hello')}</h3>
-      <button
-        type="button"
-        onClick={() => {
-          refetch();
-        }}
-      >
-        Stress test useSafeEffect
-      </button>
-      <button onClick={onLogout}>Logout</button>
       <iframe title='Frame' style={{width: '100vw', height: '100vh' }} src="https://www.spatial.io/s/NEC-BDD-1-to-1-Meeting-63ec53b91376a3a005a8ba38?share=8948596490367497350" allow="microphone https://spatial.io; camera https://spatial.io"></iframe>
     </div>
   );
